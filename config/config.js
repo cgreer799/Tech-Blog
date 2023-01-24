88f165db9cd2d238153780b744bd4607346de22c
cgreer799 <cgreer799@gmail.com>
2023-01-23 20:41:56 -0600

    updated controllers

--- config/config.js
+++ config/config.js
@@ -1,14 +1,22 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
-
-module.exports = sequelize;
+module.exports = {
+  "development": {
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": "localhost",
+      "dialect": "mysql"
+  },
+  "test": {
+      "username": "root",
+      "password": null,
+      "database": "techblog_db_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+      "dialect": "mysql"
+  }
+  };
--- controllerss/api/index.js
+++ controllerss/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
-const commentRoutes = require('./comment-routes.js');
+const userRoutes = require('./users.js');
+const postRoutes = require('./posts.js');
+const commentRoutes = require('./createcomment.js');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
