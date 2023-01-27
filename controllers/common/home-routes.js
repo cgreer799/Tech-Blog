01dd938ffbb4633b8a4a27bce3bbe250176cb883
cgreer799 <cgreer799@gmail.com>
2023-01-27 13:44:08 -0600

    removed models/index.js

--- controllers/common/home-routes.js
+++ controllers/common/home-routes.js
@@ -1,5 +1,8 @@
+const Comment = require('../../models/Comment.js');
+const Post = require('../../models/Post.js');
+const User = require('../../models/User.js');
+
 const router = require('express').Router();
-const { Post, Comment, User } = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
--- models/Index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Comment = require('./Comment');
-const Post = require('./Post');
-
-module.exports = {Comment, User, Post};
